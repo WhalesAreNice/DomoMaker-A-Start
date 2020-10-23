0469e6da79021044e4a6c2c10d11453f8e6a3421
WhalesAreNice <an1994xp@gmail.com>
2020-10-23 00:24:07 -0400

    Domomaker C finished

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -19,10 +23,29 @@ const mongooseOptions = {
 };
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
+
+//redis setup
+let redisURL = {
+    hostname: 'redis-18581.c232.us-east-1-2.ec2.cloud.redislabs.com',
+    port: '18581',
+};
+
+let redisPASS = 'hSIcF7qjdTUdybQvtzNDJaBvGhYW8VzS';
+
+if (process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    [, redisPASS] = redisURL.auth.split(':');
+}
+
+const redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPASS,
 });
 
 // pull in our routes
@@ -31,25 +54,40 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
-  extended: true,
+    extended: true,
 }));
 app.use(session({
     key: 'sessionid',
+    store: new RedisStore({
+        client: redisClient,
+    }),
     secret: 'Domo Arigato',
     resave: true,
     saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+    },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+    
+    console.log('Missing CSRF token');
+    return false;
+});
+
 router(app);
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
